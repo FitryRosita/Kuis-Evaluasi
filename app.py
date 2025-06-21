5b8ccdf3f722400b8502c9275d9f34f0c3a87b53
FitryRosita <rositadesviani@gmail.com>
2025-06-21 20:20:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -45,10 +45,10 @@ else:
 
     jawaban_pengguna = []
     for i, soal in enumerate(soal_pilgan):
-        st.markdown(f"**{i+1}.**")         # Menampilkan nomor soal tebal
-        st.markdown(soal['soal'])          # Isi soal termasuk newline & markdown
+        st.markdown(f"<b>{i+1}. {soal['soal'].replace(chr(10), '<br>')}</b>", unsafe_allow_html=True)
         jawaban = st.radio("Pilih jawaban kamu:", soal["opsi"], key=f"soal_{i}")
         jawaban_pengguna.append(jawaban.strip()[:1])
+
 
     # === Tombol submit jawaban ===
     if st.button("📨 Kirim Jawaban"):
